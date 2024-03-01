33175077d54132a398e18363bb2dc0cef4d71414
Aufer Victoriano <aufer.vicc@gmail.com>
2024-03-01 12:09:13 -0500

    First commit, VueJS basic project.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  const saludo = "¡CactusLuz!"
+</script>
+
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <h1>{{saludo}}</h1>
+  </main>
+</template>
+
+
+<style scoped>
+  h1 {
+    text-transform: uppercase;
+    color: greenyellow;
+  }
+</style>
